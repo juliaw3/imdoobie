928477e0a530d46d6d037e235ac58d956211667a
Julia Waggoner <julia@Mungo.local>
2015-10-29 13:26:54 +0000

    a somewhat less horrible header

--- moviestore/app/assets/javascripts/application.js
+++ moviestore/app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require_tree .
+//= require carts
+//= require movies
+//= require transactions
 
 $(function(){ $(document).foundation(); });
